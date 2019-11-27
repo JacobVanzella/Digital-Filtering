5dd04a204b060178368fe2ff8aae7a75be29b1f7
Jacob Vanzella <43144851+JacobVanzella@users.noreply.github.com>
2019-11-27 05:09:38 -0800

    Add files via upload

--- 362 Project/Scripts/kaiserLPF.m
+++ 362 Project/Scripts/kaiserLPF.m
@@ -4,38 +4,44 @@
 % parameter This function plots and returns the real time impulse response
 % of a Kaiser windowed low pass filter.
 
-function kaiserLPF = kaiserLPF(M, N, Fs, Fc, alpha)
-    %% Window parameters
+function hKaiserLPF = kaiserLPF(M, N, Fs, Fc, alpha)
+    %% 3.1: Window parameters
     L = 2*M;
     Z = 2*M+1;
+    n = linspace(0,L,Z)';
     
-    %% Truncated low pass filter impulse response
-    n = linspace(0,L,Z);
-    truncatedLPFImpulse = 2*(Fc/Fs)*sinc(2*Fc/Fs*(n-M));
+    %% 3.2: Filter Calculation
     
-    %% Calculation of Kaiser window
-    B = (alpha*sqrt(n.*(2*M-n) ))/M;
-    w_KaiserOr = besseli(0,B)./besseli(0,alpha);
+    % Truncated low pass filter impulse response
+    hIdealLPF = 2*(Fc/Fs)*sinc(2*Fc/Fs*(n-M));
     
-    %% Application of Kaiser window to low pass filter
-    kaiserLPF = truncatedLPFImpulse.*w_KaiserOr;
+    % Calculation of Kaiser window
+    beta = alpha*sqrt(n.*(2*M-n))/M;
+    kaiserWin = besseli(0,beta)./besseli(0,alpha);
     
-    %% Freq response calculation and plotting in dB  
-    frequency = linspace(-Fs/2,Fs/2,N);
-    DFT_KaiserLPF = fft(kaiserLPF,N);
+    % Application of Kaiser window to low pass filter
+    hKaiserLPF = hIdealLPF.*kaiserWin;
     
-    %% Plot of time domain Kaiser LPF
+    % Freq response calculation and plotting in dB
+    HKaiserLPF = fft(hKaiserLPF,N);
+    HZeroKaiserLPF = fftshift(HKaiserLPF);
+    frequency = linspace(-Fs/2,Fs/2,N)';
+    
+    %% 3.3: Kaiser Low-Pass Filter Plots
+    
+    % Plot of time domain Kaiser LPF
     figure;
-    subplot(2,1,1)
-    plot(n,w_KaiserOr,'g')
-    xlabel('n')
-    ylabel ('Kaiser Window function')
-    ylim([-inf,1.1]);
+    subplot(2,1,1);
+    plot(n,hKaiserLPF);
+    xlabel('n');
+    ylabel ('h(n)');
+    title('Kaiser Windowed LPF Time Response');
     
-    %% Plot of frequency domain Kaiser LPF
-    subplot(2,1,2)
-    plot(frequency,fftshift(20*log10(abs(DFT_KaiserLPF))),'g');
-    xlabel('Frequency')
-    ylabel('Response Amplitude (dB)')
+    % Plot of frequency domain Kaiser LPF
+    subplot(2,1,2);
+    plot(frequency,real(20*log10(HZeroKaiserLPF)));
+    xlabel('Frequency');
+    ylabel('H(f) (dB)');
+    title('Kaiser Windowed LPF Frequency Response');
     ylim([-inf,10]);
 end
