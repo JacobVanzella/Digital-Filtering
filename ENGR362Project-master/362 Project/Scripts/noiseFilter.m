b5451752f5f2275322864bf94dcc4ab03cf6eba9
Jacob Vanzella <JacobVanzella@users.noreply.github.com>
2020-01-17 22:30:44 -0800

    Cleanup
    Project cleaned of junk files.

--- ENGR362Project-master/362 Project/Scripts/noiseFilter.m
+++
@@ -1,7 +0,0 @@
-% Noise filter accepts the the discrete fourier transform (DFT) of a
-% signal and two filters. These filters are applied to the DFT, and the
-% inverse DFT is returned.
-function cleanRec = noiseFilter(signal, filter1, filter2)
-    cleanRec = filtfilt(filter1,1,signal);
-    cleanRec = filtfilt(filter2,1,cleanRec);
-end
